514363e2d8e1e15cdcdf1391bd463976cc7c9b79
ram <24ram46r@gmail.com>
2023-12-25 21:00:15 +0530

    terraform file

--- main.tf
+++ main.tf
@@ -1,18 +1,78 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
+
   region = "ap-south-1"
 }
 
-resource "aws_instance" "my_instance" {
+// use this if you create a terraform user instead of aws configure.
+// provider "aws" {
+//   region     = "AWS_REGION"
+//   access_key = "AWS_ACCESS_KEY"
+//   secret_key = "AWS_SECRET_KEY"
+// }
 
-  ami           = "ami-03f4878755434977f"
-  instance_type = "t3.micro"
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 20
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+variable "key_name" {
+  description = "ram_ec2"
+}
+
+// Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+// Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+# Create a security group
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-03f4878755434977f"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+
   tags = {
+    Name = "public_instance"
+  }
 
-    "Name" = "my_instance14"
-
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
   }
 }
